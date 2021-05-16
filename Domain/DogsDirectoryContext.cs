cfc554e2f2b49e3a318974d65a41534a1f19058f
Pablo <pablo@E195.local>
2021-05-16 08:57:26 -0300

    Agrego archivos de EF

---
+++ Domain/DogsDirectoryContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+
+namespace Domain
+{
+    public class DogsDirectoryContext : DbContext
+    {
+        public DbSet<DogOwner> Owners { get; set; }
+        public DbSet<Dog> Dogs { get; set; }
+    }
+}
---
+++ Domain/DogsDirectorySqlServer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class DogsDirectorySqlServer
+    {
+        public void AddOwner(DogOwner aDogOwner)
+        {
+            using (DogsDirectoryContext dbContext = new DogsDirectoryContext())
+            {
+                dbContext.Owners.Add(aDogOwner);
+
+                dbContext.SaveChanges();
+            }
+        }
+
+        public IEnumerable<DogOwner> Owners()
+        {
+            using (DogsDirectoryContext dbContext = new DogsDirectoryContext())
+            {
+                return dbContext.Owners.ToList();
+            }
+        }
+
+        public void AddDog(DogOwner aDogOwner, Dog aDog)
+        {
+            using (DogsDirectoryContext dbContext = new DogsDirectoryContext())
+            {
+                aDog.Owner = dbContext.Owners.First(owner => owner.Id == aDogOwner.Id);
+
+                dbContext.Dogs.Add(aDog);
+
+                dbContext.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Dog> DogsForOwner(DogOwner aDogOwner)
+        {
+            using (DogsDirectoryContext dbContext = new DogsDirectoryContext())
+            {
+                var dogs = dbContext.Dogs
+                     .Where(dog => dog.Owner.Id == aDogOwner.Id)
+                     .ToList();
+
+                return dogs;
+            }
+        }
+    }
+}
